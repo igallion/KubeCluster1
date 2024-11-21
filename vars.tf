17e8b0467c8cdb3bca62fc932d43ab744695ebd7
igallion <82122432+igallion@users.noreply.github.com>
2024-11-21 15:42:56 -0500

    Initial Commit

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "ssh_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDChDEw8LZTAddeWB8XaWuwGIQsyHWFuYa135RVfuie/J2ZPrW9ExqBHiNJBuYV5QkvJvK8Iafa5/X0jcyFMh+UrTBMHoVU5uf5bveYt86iMO2eTNKjeEXeRyZimmqHBvxvjIWRTvJ3z4V9Ngi9+pPfw+kUFgYcTh/T134SuWcLghBZsycw0upQ0P61iSt0rt0yqrBbYrIey81d5Br8gTo+SdbrORT/FQpUUJcnGwGjXt++JhWlDJLNIenquXs1Do1/HiM65YqnPaWaOvNlXtxUbExWIS9qeJ8vEirdiWthWEB+HpFg8teosFobZ9BErsztcIko7Z/+3CyL8r7KCpzMiQsr7pMnhbQRZV7uzoW1+JS9SRE9X6eeoZUhSAYbVsf0u1VE3HXaVYTr3Cs3XeSrftKTMjNFAmETW7O3pt4l97pOFjt5bWE1+AGGgtWobT9u4ngdInZlLU+aYy3q7sRL54kvVmJWpquinBJfSzJrh+ZOLCRC22xf84eCo3x3tVU= isaacgallion@Isaacs-MacBook-Air.local"
+}
+variable "proxmox_host" {
+  default = "PMHost02"
+}
+
+variable "template_name" {
+  default = "Ubuntu-Latest-Template"
+}
